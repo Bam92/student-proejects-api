2158cd73cec16b0d0fad1566b92009bc8371a3cd
NdekoCode <arickbulakali@gmail.com>
2023-02-02 15:52:33 +0200

    setup: add test config and initialize user controller

---
+++ test/users.test.js
@@ -0,0 +1,24 @@
+import { hash } from "bcrypt";
+import { BASE_API } from "../utils/constants.js";
+import request from "./setup.test.js";
+describe("Should regist a user", async (done) => {
+    const password = await hash("7288", 12);
+    const user = {
+        firstName: "Cedric",
+        lastName: "Kahungu",
+        email: "cedrickahungu@gmail.com",
+        password,
+    };
+    request
+        .post(BASE_API + "/auth/register")
+        .send(user)
+        .then((res) => {
+            expect(res.body).to.be.a("object");
+            expect(res.status).to.be.equal(201);
+            done();
+        })
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
+});
