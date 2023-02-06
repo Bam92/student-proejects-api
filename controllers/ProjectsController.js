e07b2e454856e165ac6dbf0c1330f91f13a07e17
NdekoCode <arickbulakali@gmail.com>
2023-02-06 18:48:44 +0200

    get single project

--- controllers/ProjectsController.js
+++ controllers/ProjectsController.js
@@ -13,5 +13,17 @@ export default class ProjectsController {
     }
     async getOneProject(req, res) {
         const id = req.params.id;
+        const alert = new Alert(req, res);
+        try {
+            const project = await Project.findByPk(id);
+            if (project) {
+                return res.status(200).json(project);
+            }
+
+            return alert.danger("Le projet est introuvable", 400);
+        } catch (error) {
+            console.log(error);
+            return alert.danger(error.message, 500);
+        }
     }
 }
